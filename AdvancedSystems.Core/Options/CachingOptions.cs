9ee6dc5747c00204b0b0004ce144990b47a1a81a
StefanGreve <greve.stefan@outlook.jp>
2024-07-09 19:14:15 +0200

    Add helper methods for dependency injection

---
+++ AdvancedSystems.Core/Options/CachingOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace AdvancedSystems.Core.Options;
+
+public sealed class CachingOptions
+{
+    #region Properties
+
+    public string? Configuration { get; init; }
+
+    public string? InstanceName { get; init; }
+
+    #endregion
+}
