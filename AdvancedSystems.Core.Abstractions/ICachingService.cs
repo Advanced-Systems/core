a7ae4424643bc8adb44d425b390ffeb1c1b241f7
StefanGreve <greve.stefan@outlook.jp>
2024-07-09 17:39:15 +0200

    Implement CachingService

---
+++ AdvancedSystems.Core.Abstractions/ICachingService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdvancedSystems.Core.Abstractions
+{
+    /// <summary>
+    ///     Represents a cache of serialized values.
+    /// </summary>
+    public interface ICachingService
+    {
+        /// <summary>
+        ///     Sets a values in the cache asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The type argument must be a reference type and  have a public parameterless constructor.</typeparam>
+        /// <param name="key">A string identifying the requested values.</param>
+        /// <param name="value">The values to set in the cache.</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be cancelled.</param>
+        /// <returns>A ValueTask representing the asynchronous operation.</returns>
+        /// <exception cref="ArgumentNullException">The parameter <paramref name="key"/> is <c>null</c>.</exception>
+        /// <exception cref="OperationCanceledException">The cancellation token was canceled. This exception is stored into the returned task.</exception>
+        ValueTask SetAsync<T>(string? key, T value, CancellationToken cancellationToken = default) where T : class, ICacheable, new();
+
+        /// <summary>
+        ///     Gets a values from the cache asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The type argument must be a reference type and  have a public parameterless constructor.</typeparam>
+        /// <param name="key">A string identifying the requested values.</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be cancelled.</param>
+        /// <returns>A ValueTask containing the result of type <typeparamref name="T"/> representing the asynchronous operation. The result is null if <paramref name="key"/> can not be identified in the cache.</returns>
+        /// <exception cref="ArgumentNullException">The parameter <paramref name="key"/> is <c>null</c>.</exception>
+        /// <exception cref="OperationCanceledException">The cancellation token was canceled. This exception is stored into the returned task.</exception>
+        ValueTask<T?> GetAsync<T>(string? key, CancellationToken cancellationToken = default) where T : class, ICacheable, new();
+    }
+}
---
+++ AdvancedSystems.Core/Services/CachingService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using AdvancedSystems.Core.Abstractions;
+using AdvancedSystems.Security.Common;
+
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace AdvancedSystems.Core.Services;
+
+public sealed class CachingService : ICachingService
+{
+    private readonly IDistributedCache _distributedCache;
+
+    public CachingService(IDistributedCache distributedCache)
+    {
+        this._distributedCache = distributedCache;
+    }
+
+    #region Methods
+
+    public async ValueTask SetAsync<T>(string? key, T value, CancellationToken cancellationToken = default) where T : class, ICacheable, new()
+    {
+        ArgumentNullException.ThrowIfNull(key, nameof(key));
+        byte[] cacheValue = ObjectSerializer.Serialize(value).ToArray();
+        var options = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpiration = value?.AbsoluteExpiration,
+            AbsoluteExpirationRelativeToNow = value?.AbsoluteExpirationRelativeToNow,
+            SlidingExpiration = value?.SlidingExpiration,
+        };
+
+        await this._distributedCache.SetAsync(key, cacheValue, options, cancellationToken);
+    }
+
+    public async ValueTask<T?> GetAsync<T>(string? key, CancellationToken cancellationToken = default) where T : class, ICacheable, new()
+    {
+        ArgumentNullException.ThrowIfNull(key, nameof(key));
+        byte[]? cachedValues = await this._distributedCache.GetAsync(key, cancellationToken);
+
+        if (cachedValues == null || cachedValues.Length == 0) return default;
+
+        T @object = ObjectSerializer.Deserialize<T>(cachedValues);
+        return @object;
+    }
+
+    #endregion
+
+}
