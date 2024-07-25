b087474eb6d4ac0d3c3ddaa90269846d743e870c
StefanGreve <greve.stefan@outlook.jp>
2024-07-25 22:29:41 +0200

    Remove ICacheable interface and and CacheOptions in its place
    * Turn AdvancedSystems.Core.Asbtractions to a net8.0 project
    * Use file-scoped namespaces

---
+++ AdvancedSystems.Core.Abstractions/CacheOptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace AdvancedSystems.Core.Abstractions;
+
+public class CacheOptions
+{
+    /// <summary>
+    ///     Provides the cache options for an entry in <seealso cref="ICachingService"/>.
+    /// </summary>
+    public DateTimeOffset? AbsoluteExpiration { get; set; }
+
+    /// <summary>
+    ///     Gets or sets an absolute expiration time, relative to now.
+    /// </summary>
+    public TimeSpan? AbsoluteExpirationRelativeToNow { get; set; }
+
+    /// <summary>
+    /// Gets or sets how long a cache entry can be inactive (e.g. not accessed) before it will be removed.
+    /// This will not extend the entry lifetime beyond the absolute expiration (if set).
+    /// </summary>
+    public TimeSpan? SlidingExpiration { get; set; }
+
+    public static implicit operator DistributedCacheEntryOptions(CacheOptions options)
+    {
+        return new DistributedCacheEntryOptions
+        {
+            AbsoluteExpiration = options.AbsoluteExpiration,
+            AbsoluteExpirationRelativeToNow = options.AbsoluteExpirationRelativeToNow,
+            SlidingExpiration = options.SlidingExpiration,
+        };
+    }
+}
