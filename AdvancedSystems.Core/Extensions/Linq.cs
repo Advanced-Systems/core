546ce24aefc2e51ebe5926f8f89d01950327fe3d
StefanGreve <greve.stefan@outlook.jp>
2024-08-05 22:12:36 +0200

    Implement IsNullOrEmpty extension method

---
+++ AdvancedSystems.Core/Extensions/Linq.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdvancedSystems.Core.Extensions;
+
+public static class Linq
+{
+    /// <summary>
+    ///     Checks whether enumerable is null or empty.
+    /// </summary>
+    /// <typeparam name="T">The type of the enumerable.</typeparam>
+    /// <param name="enumerable">The enumerable to be checked.</param>
+    /// <returns> True if enumerable is null or empty, false otherwise.</returns>
+    public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+    {
+        return enumerable == null || !enumerable.Any();
+    }
+}
