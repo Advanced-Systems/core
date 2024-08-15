0b1891410daa6c323381f969e6c8ee5095d41bfd
StefanGreve <greve.stefan@outlook.jp>
2024-08-15 22:11:32 +0200

    Fix code formatting

--- AdvancedSystems.Core.Abstractions/CacheOptions.cs
+++ AdvancedSystems.Core.Abstractions/CacheOptions.cs
@@ -17,8 +17,8 @@ public class CacheOptions
     public TimeSpan? AbsoluteExpirationRelativeToNow { get; set; }
 
     /// <summary>
-    /// Gets or sets how long a cache entry can be inactive (e.g. not accessed) before it will be removed.
-    /// This will not extend the entry lifetime beyond the absolute expiration (if set).
+    ///     Gets or sets how long a cache entry can be inactive (e.g. not accessed) before it will be removed.
+    ///     This will not extend the entry lifetime beyond the absolute expiration (if set).
     /// </summary>
     public TimeSpan? SlidingExpiration { get; set; }
 
