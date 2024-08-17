b114c180c47fcdec3b12322a78c7e5131ae21d84
StefanGreve <greve.stefan@outlook.jp>
2024-08-17 22:00:38 +0200

    Update and extend unit tests for MessageBus service

--- AdvancedSystems.Core.Tests/Services/MessageBusTests.cs
+++ AdvancedSystems.Core.Tests/Services/MessageBusTests.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 using AdvancedSystems.Core.Abstractions;
+using AdvancedSystems.Core.DependencyInjection;
 using AdvancedSystems.Core.Tests.Fixtures;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 using Xunit;
 
@@ -19,7 +26,7 @@ public class MessageBusTests : IClassFixture<MessageBusFixture>
 
     public record Message(Guid Id) : IMessage;
 
-    public record Passport(Guid Id) : IMessage;
+    public record Passport(string FirstName, string LastName, Guid Id) : IMessage;
 
     #region Tests
 
@@ -29,10 +36,12 @@ public class MessageBusTests : IClassFixture<MessageBusFixture>
         // Arrange
         var messageBus = this._fixture.MessageBus;
         var expected = new Message(Guid.NewGuid());
+        string channelName = "internal";
 
         // Act
+        messageBus.Register(channelName);
         await messageBus.PublishAsync(expected);
-        Message actual = await messageBus.SubscribeAsync<Message>();
+        Message actual = await messageBus.SubscribeAsync<Message>(channelName);
 
         // Assert
         Assert.Equal(expected.Id, actual.Id);
@@ -44,17 +53,63 @@ public class MessageBusTests : IClassFixture<MessageBusFixture>
         // Arrange
         var messageBus = this._fixture.MessageBus;
         var expected = new Message(Guid.NewGuid());
+        string channelName = "internal";
+        string topic = "test";
 
         // Act
-        await messageBus.PublishAsync(expected);
+        messageBus.Register(channelName, topic);
+        await messageBus.PublishAsync(expected, topic, CancellationToken.None);
 
-        async Task SubscribeAsync()
+        Passport? actual = await messageBus.SubscribeAsync<Passport>(channelName, default, CancellationToken.None);
+
+        // Assert
+        Assert.Null(actual);
+    }
+
+    [Fact]
+    public async Task TestPublishAsync_ThrowExceptionWithoutRegistration()
+    {
+        // Arrange
+        var messageBus = this._fixture.MessageBus;
+        var expected = new Message(Guid.NewGuid());
+
+        // Act
+        async Task PublishAsync()
         {
-            await messageBus.SubscribeAsync<Passport>();
+            await messageBus.PublishAsync(expected);
         }
 
         // Assert
-        await Assert.ThrowsAsync<InvalidCastException>(SubscribeAsync);
+        await Assert.ThrowsAsync<InvalidOperationException>(PublishAsync);
+    }
+
+    [Fact]
+    public async Task TestAddMessageBus()
+    {
+        // Arrange
+        using var hostBuilder = await new HostBuilder()
+            .ConfigureWebHost(builder =>
+            {
+                builder.UseTestServer();
+                builder.ConfigureServices(services =>
+                {
+#pragma warning disable Preview008 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+                    services.AddMessageBus();
+#pragma warning restore Preview008 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+                });
+                builder.Configure(app =>
+                {
+
+                });
+            })
+            .StartAsync();
+
+        // Act
+        var messageBus = hostBuilder.Services.GetService<IMessageBus>();
+
+        // Assert
+        Assert.NotNull(messageBus);
+        await hostBuilder.StopAsync();
     }
 
     #endregion
