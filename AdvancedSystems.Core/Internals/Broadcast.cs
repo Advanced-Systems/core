d221e1d133260f0a3d970788706d990ab76f8450
StefanGreve <greve.stefan@outlook.jp>
2024-08-17 21:59:46 +0200

    Add broadcasting capabilities to message bus

---
+++ AdvancedSystems.Core/Internals/Broadcast.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Channels;
+
+namespace AdvancedSystems.Core.Internals;
+
+internal class Broadcast<T>
+{
+    internal required string Name { get; init; }
+
+    internal required Channel<T> Channel { get; init; }
+
+    internal string? Topic { get; init; }
+}
