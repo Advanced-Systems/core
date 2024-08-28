fc21c7bd79d8508c4166830ff3f22681a7cdee3f
StefanGreve <greve.stefan@outlook.jp>
2024-08-28 18:28:03 +0200

    Implement AddCompressionService

---
+++ AdvancedSystems.Core/Services/CompressionService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO.Compression;
+
+using AdvancedSystems.Core.Abstractions;
+using AdvancedSystems.Core.Common;
+
+using Microsoft.Extensions.Logging;
+
+namespace AdvancedSystems.Core.Services;
+
+public sealed class CompressionService : ICompressionService
+{
+    private readonly ILogger<CompressionService> _logger;
+
+    public CompressionService(ILogger<CompressionService> logger)
+    {
+        this._logger = logger;
+    }
+
+    #region Methods
+
+    public byte[] Compress(ReadOnlyMemory<byte> expandedBuffer, CompressionLevel compressionLevel)
+    {
+        this._logger.LogTrace("Compression Level = {CompressionLevel}", compressionLevel);
+        return Archive.Compress(expandedBuffer, compressionLevel);
+    }
+
+    public byte[] Expand(ReadOnlyMemory<byte> compressedBuffer)
+    {
+        return Archive.Expand(compressedBuffer);
+    }
+
+    #endregion
+}
