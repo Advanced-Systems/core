861b2e29cc1fad52e453b0bd96afb98a93f76097
StefanGreve <greve.stefan@outlook.jp>
2024-08-28 18:28:17 +0200

    Add unit tests for CompressionService

---
+++ AdvancedSystems.Core.Tests/Fixtures/CompressionServiceFixture.cs
@@ -0,0 +1,24 @@
+﻿using AdvancedSystems.Core.Abstractions;
+using AdvancedSystems.Core.Services;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+namespace AdvancedSystems.Core.Tests.Fixtures;
+
+public class CompressionServiceFixture
+{
+    public CompressionServiceFixture()
+    {
+        this.CompressionService = new CompressionService(this.Logger.Object);
+    }
+
+    #region Properties
+
+    public Mock<ILogger<CompressionService>> Logger { get; private set; } = new();
+
+    public ICompressionService CompressionService { get; private set; }
+
+    #endregion
+}
