ed3c498487c846156a9b11084911c34ef0c00b00
StefanGreve <greve.stefan@outlook.jp>
2024-08-28 11:41:52 +0200

    Fix bugs and simplify API surface

--- AdvancedSystems.Core/Common/Archive.cs
+++ AdvancedSystems.Core/Common/Archive.cs
@@ -1,77 +1,46 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
-using System.Runtime.InteropServices;
 
 namespace AdvancedSystems.Core.Common;
 
 public static class Archive
 {
-    public static byte[] Compress(Stream expandedStream, CompressionLevel compressionLevel)
-    {
-        using var compressedStream = new MemoryStream();
-
-        using var gzipStream = new GZipStream(expandedStream, compressionLevel, leaveOpen: false);
-        expandedStream.CopyTo(gzipStream);
-
-        return compressedStream.ToArray();
-    }
-
     public static byte[] Compress(ReadOnlyMemory<byte> expandedBuffer, CompressionLevel compressionLevel)
     {
+        if (expandedBuffer.IsEmpty) throw new ArgumentException("Buffer to compress cannot be empty.", nameof(expandedBuffer));
         using var compressedStream = new MemoryStream();
-        GCHandle handle = GCHandle.Alloc(expandedBuffer, GCHandleType.Pinned);
 
-        try
+        unsafe
         {
-            unsafe
+            fixed (byte* pointer = &expandedBuffer.Span[0])
             {
-                IntPtr pointer = handle.AddrOfPinnedObject();
-                using var expandedStream = new UnmanagedMemoryStream((byte*)pointer, expandedBuffer.Length);
+                using var expandedStream = new UnmanagedMemoryStream(pointer, expandedBuffer.Length);
 
                 using var gzipStream = new GZipStream(compressedStream, compressionLevel, leaveOpen: false);
                 expandedStream.CopyTo(gzipStream);
             }
-
-            return compressedStream.ToArray();
         }
-        finally
-        {
-            handle.Free();
-        }
-    }
 
-    public static byte[] Expand(Stream compressedStream)
-    {
-        using var uncompressedStream = new MemoryStream();
-
-        using var gzipStream = new GZipStream(compressedStream, CompressionMode.Decompress, leaveOpen: false);
-        gzipStream.CopyTo(uncompressedStream);
-
-        return uncompressedStream.ToArray();
+        return compressedStream.ToArray();
     }
 
     public static byte[] Expand(ReadOnlyMemory<byte> compressedBuffer)
     {
+        if (compressedBuffer.IsEmpty) throw new ArgumentException("Buffer to expand cannot be empty.", nameof(compressedBuffer));
         using var expandedStream = new MemoryStream();
-        GCHandle handle = GCHandle.Alloc(compressedBuffer, GCHandleType.Pinned);
-
-        try
+        
+        unsafe
         {
-            unsafe
+            fixed (byte* pointer = &compressedBuffer.Span[0])
             {
-                IntPtr pointer = handle.AddrOfPinnedObject();
-                using var compressedStream = new UnmanagedMemoryStream((byte*)pointer, compressedBuffer.Length);
+                using var compressedStream = new UnmanagedMemoryStream(pointer, compressedBuffer.Length);
 
                 using var gzipStream = new GZipStream(compressedStream, CompressionMode.Decompress, leaveOpen: false);
                 gzipStream.CopyTo(expandedStream);
             }
+        }
 
-            return expandedStream.ToArray();
-        }
-        finally
-        {
-            handle.Free();
-        }
+        return expandedStream.ToArray();
     }
 }
