864848b85c78eb7e0731d853a693ab3a631a231b
StefanGreve <greve.stefan@outlook.jp>
2024-08-29 12:05:43 +0200

    Add required jsonTypeInfo parameter to all ICachingService methods

--- AdvancedSystems.Core.Abstractions/ICachingService.cs
+++ AdvancedSystems.Core.Abstractions/ICachingService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Text.Json.Serialization.Metadata;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AdvancedSystems.Core.Abstractions;
@@ -8,25 +9,7 @@ namespace AdvancedSystems.Core.Abstractions;
 /// </summary>
 public interface ICachingService
 {
-    /// <summary>
-    ///     Sets a values in the cache asynchronously.
-    /// </summary>
-    /// <typeparam name="T">
-    ///     The type argument must be a reference type and  have a public parameterless constructor.
-    /// </typeparam>
-    /// <param name="key">
-    ///     A string identifying the requested values.
-    /// </param>
-    /// <param name="value">
-    ///     The values to set in the cache.
-    /// </param>
-    /// <param name="cancellationToken">
-    ///     Propagates notification that operations should be cancelled.
-    /// </param>
-    /// <returns>
-    ///     A ValueTask representing the asynchronous operation.
-    /// </returns>
-    ValueTask SetAsync<T>(string key, T value, CancellationToken cancellationToken = default) where T : class;
+    #region Methods
 
     /// <summary>
     ///     Sets a values in the cache asynchronously.
@@ -39,6 +22,32 @@ public interface ICachingService
     /// </param>
     /// <param name="value">
     ///     The values to set in the cache.
+    /// </param>
+    /// <param name="jsonTypeInfo">
+    ///     The metadata for the specified type.
+    /// </param>
+    /// <param name="cancellationToken">
+    ///     Propagates notification that operations should be cancelled.
+    /// </param>
+    /// <returns>
+    ///     A ValueTask representing the asynchronous operation.
+    /// </returns>
+    ValueTask SetAsync<T>(string key, T value, JsonTypeInfo<T> jsonTypeInfo, CancellationToken cancellationToken = default) where T : class;
+
+    /// <summary>
+    ///     Sets a values in the cache asynchronously.
+    /// </summary>
+    /// <typeparam name="T">
+    ///     The type argument must be a reference type and  have a public parameterless constructor.
+    /// </typeparam>
+    /// <param name="key">
+    ///     A string identifying the requested values.
+    /// </param>
+    /// <param name="value">
+    ///     The values to set in the cache.
+    /// </param>
+    /// <param name="jsonTypeInfo">
+    ///     The metadata for the specified type.
     /// </param>
     /// <param name="options">
     ///     The cache options for the value.
@@ -49,7 +58,7 @@ public interface ICachingService
     /// <returns>
     ///     A ValueTask representing the asynchronous operation.
     /// </returns>
-    ValueTask SetAsync<T>(string key, T value, CacheOptions options, CancellationToken cancellationToken = default) where T : class;
+    ValueTask SetAsync<T>(string key, T value, JsonTypeInfo<T> jsonTypeInfo, CacheOptions options, CancellationToken cancellationToken = default) where T : class;
 
     /// <summary>
     ///     Gets a values from the cache asynchronously.
@@ -60,6 +69,9 @@ public interface ICachingService
     /// <param name="key">
     ///     A string identifying the requested values.
     /// </param>
+    /// <param name="jsonTypeInfo">
+    ///     The metadata for the specified type.
+    /// </param>
     /// <param name="cancellationToken">
     ///     Propagates notification that operations should be cancelled.
     /// </param>
@@ -67,5 +79,7 @@ public interface ICachingService
     ///     A ValueTask containing the result of type <typeparamref name="T"/> representing the asynchronous
     ///     operation. The result is null if <paramref name="key"/> can not be identified in the cache.
     /// </returns>
-    ValueTask<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default) where T : class;
+    ValueTask<T?> GetAsync<T>(string key, JsonTypeInfo<T> jsonTypeInfo, CancellationToken cancellationToken = default) where T : class;
+
+    #endregion
 }
