f0bde187e6a3c19ce5b8955d4d483d688cbb7360
StefanGreve <greve.stefan@outlook.jp>
2024-08-30 12:07:51 +0200

    Fix unit tests

---
+++ AdvancedSystems.Core.Tests/Common/ObjectSerializerTests.cs
@@ -0,0 +1,28 @@
+﻿using AdvancedSystems.Core.Common;
+using AdvancedSystems.Core.Tests.Models;
+
+using Xunit;
+
+namespace AdvancedSystems.Core.Tests.Common;
+
+public class ObjectSerializerTests
+{
+    #region Tests
+
+    [Fact]
+    public void TestSerializationRoundtrip_HappyPath()
+    {
+        // Arrange
+        var expected = new Person("Stefan", "Greve");
+
+        // Act
+        var serialized = ObjectSerializer.Serialize(expected, PersonContext.Default.Person);
+        Person? actual = ObjectSerializer.Deserialize(serialized, PersonContext.Default.Person);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.Equal(expected, actual);
+    }
+
+    #endregion
+}
---
+++ AdvancedSystems.Core.Tests/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AdvancedSystems.Core.Tests.Models;
+
+internal record Person(string FirstName, string LastName);
+
+[JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Default)]
+[JsonSerializable(typeof(Person))]
+internal partial class PersonContext : JsonSerializerContext;
