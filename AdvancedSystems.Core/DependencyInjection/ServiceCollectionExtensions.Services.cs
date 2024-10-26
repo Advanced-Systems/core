b34d6aaef229b7559a0eba3a64914dd831eba90a
StefanGreve <greve.stefan@outlook.jp>
2024-10-26 14:19:50 +0200

    Add doc strings to all other ServiceCollectionExtensions methods

--- AdvancedSystems.Core/DependencyInjection/ServiceCollectionExtensions.Services.cs
+++ AdvancedSystems.Core/DependencyInjection/ServiceCollectionExtensions.Services.cs
@@ -3,6 +3,7 @@
 using AdvancedSystems.Core.Abstractions;
 using AdvancedSystems.Core.Services;
 
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -10,6 +11,18 @@ namespace AdvancedSystems.Core.DependencyInjection;
 
 public static partial class ServiceCollectionExtensions
 {
+    /// <summary>
+    ///     Adds the default implementation of <seealso cref="ICachingService"/> to <paramref name="services"/>.
+    /// </summary>
+    /// <param name="services">
+    ///     The service collection containing the service.
+    /// </param>
+    /// <returns>
+    ///     The value of <paramref name="services"/>.
+    /// </returns>
+    /// <remarks>
+    ///     To use this service, you must configure an <seealso cref="IDistributedCache"/> provider separately.
+    /// </remarks>
     public static IServiceCollection AddCachingService(this IServiceCollection services)
     {
         services.AddSerializationService();
@@ -17,12 +30,30 @@ public static partial class ServiceCollectionExtensions
         return services;
     }
 
+    /// <summary>
+    ///     Adds the default implementation of <seealso cref="ICompressionService"/> to <paramref name="services"/>.
+    /// </summary>
+    /// <param name="services">
+    ///     The service collection containing the service.
+    /// </param>
+    /// <returns>
+    ///     The value of <paramref name="services"/>.
+    /// </returns>
     public static IServiceCollection AddCompressionService(this IServiceCollection services)
     {
         services.TryAdd(ServiceDescriptor.Scoped<ICompressionService, CompressionService>());
         return services;
     }
 
+    /// <summary>
+    ///     Adds the default implementation of <seealso cref="IMessageBus"/> to <paramref name="services"/>.
+    /// </summary>
+    /// <param name="services">
+    ///     The service collection containing the service.
+    /// </param>
+    /// <returns>
+    ///     The value of <paramref name="services"/>.
+    /// </returns>
     [Experimental("Preview008")]
     public static IServiceCollection AddMessageBus(this IServiceCollection services)
     {
@@ -30,6 +61,15 @@ public static partial class ServiceCollectionExtensions
         return services;
     }
 
+    /// <summary>
+    ///     Adds the default implementation of <seealso cref="ISerializationService"/> to <paramref name="services"/>.
+    /// </summary>
+    /// <param name="services">
+    ///     The service collection containing the service.
+    /// </param>
+    /// <returns>
+    ///     The value of <paramref name="services"/>.
+    /// </returns>
     public static IServiceCollection AddSerializationService(this IServiceCollection services)
     {
         services.TryAdd(ServiceDescriptor.Scoped<ISerializationService, SerializationService>());
